65ca24f61a70d66d846ad9f06cf961803137387f
tom <chings228@gmail.com>
2023-07-10 09:31:40 +0800

    Initial Commit

---
+++ SpeakingTraining/SpeakingTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakingTrainingApp.swift
+//  SpeakingTraining
+//
+//  Created by man ching chan on 10/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeakingTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
