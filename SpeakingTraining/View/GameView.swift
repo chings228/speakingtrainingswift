3a939b0608c653de9247f7b907a4b905fb03c6cf
Tom <chings228@gmail.com>
2023-07-23 16:09:26 +0800

    add game view

---
+++ SpeakingTraining/View/GameView.swift
@@ -0,0 +1,26 @@
+//
+//  GameView.swift
+//  SpeakingTraining
+//
+//  Created by man ching chan on 23/7/2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    
+    
+    
+    @EnvironmentObject 
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
